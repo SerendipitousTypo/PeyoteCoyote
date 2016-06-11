3b1371333465da2cd979b68d0752f3fb5b9228b1
Elbertlum <elbert.lum@gmail.com>
2016-06-11 14:48:40 -0700

    change localhost to deployed ip

--- roam/App/Components/Time.js
+++ roam/App/Components/Time.js
@@ -56,9 +56,8 @@ class Time extends Component {
 
     this.nav('Confirmation', this.props.userEmail);
 
-    // fetch('http://107.170.251.113:3000/roam', {
-
-    fetch('http://localhost:3000/roam', {
+    fetch('http://107.170.251.113:3000/roam', {
+    // fetch('http://localhost:3000/roam', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
